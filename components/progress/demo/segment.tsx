bcdff8f5acd02da07a6278279af0bcb534dee3cf
suoyine <50694715+suoyine@users.noreply.github.com>
2026-05-22 16:22:47 +0800

    Update segment.tsx
    Signed-off-by: suoyine <50694715+suoyine@users.noreply.github.com>

--- components/progress/demo/segment.tsx
+++ components/progress/demo/segment.tsx
@@ -3,15 +3,15 @@ import { Flex, Progress, Tooltip } from 'antd';
 
 const App: React.FC = () => (
   <Flex gap="small" vertical>
-    <Tooltip title="3 done / 3 in progress / 4 to do">
-      <Progress percent={60} success={{ percent: 30 }} />
+    <Tooltip title="3 done / 3 in progress / 1 failed / 3 to do">
+      <Progress percent={60} success={{ percent: 30 }} failed={{ percent: 10 }} />
     </Tooltip>
     <Flex gap="small" wrap>
-      <Tooltip title="3 done / 3 in progress / 4 to do">
-        <Progress percent={60} success={{ percent: 30 }} type="circle" />
+      <Tooltip title="3 done / 3 in progress / 1 failed / 3 to do">
+        <Progress percent={60} success={{ percent: 30 }} failed={{ percent: 10 }} type="circle" />
       </Tooltip>
       <Tooltip title="3 done / 3 in progress / 4 to do">
-        <Progress percent={60} success={{ percent: 30 }} type="dashboard" />
+        <Progress percent={60} success={{ percent: 30 }} failed={{ percent: 10 }} type="dashboard" />
       </Tooltip>
     </Flex>
   </Flex>
